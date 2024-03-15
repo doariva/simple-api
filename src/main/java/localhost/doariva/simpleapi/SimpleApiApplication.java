01101ba531eb332ed803a33b914c890565269c2f
doariva <doariva.0@gmail.com>
2024-03-16 00:18:47 +0900

    init spring boot project

---
+++ src/main/java/localhost/doariva/simpleapi/SimpleApiApplication.java
@@ -0,0 +1,15 @@
+package localhost.doariva.simpleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiApplication.class, args);
+	}
+
+}
