4bd03f1b39d2a70c67129437f9573f057819b6d0
Your Name <your_email@example.com>
2025-06-28 05:03:30 +0000

    feat: Add tests for add and subtract endpoints

--- src/test/java/localhost/doariva/simpleapi/SimpleApiApplicationTests.java
+++ src/test/java/localhost/doariva/simpleapi/SimpleApiApplicationTests.java
@@ -1,13 +1,30 @@
 package localhost.doariva.simpleapi;
 
+import localhost.doariva.simpleapi.controller.HelloController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SimpleApiApplicationTests {
 
+	@Autowired
+	private HelloController helloController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void add() {
+		assertEquals(5, helloController.add(2, 3));
+	}
+
+	@Test
+	void subtract() {
+		assertEquals(1, helloController.subtract(3, 2));
 	}
 
 }
